f3869f4e275d35f99b7bdd60e86473fd8786a149
emekaJoseph <josephiwuji@gmail.com>
2021-03-10 17:05:39 +0100

    save a code

--- public/Scripts/Admin/home.js
+++ public/Scripts/Admin/home.js
@@ -19,92 +19,68 @@
 //     }
 // }
 
-// // for image 2
-// var input = document.getElementById('avatar_2');
-// if (input) {
-//     input.addEventListener('change', function () {
-//         getUploadImageUrl_2(this);
-//         //sendImage();
-//     });
+
+
+
+
+//Newly untried:
+
+// const MAX_WIDTH = 320;
+// const MAX_HEIGHT = 180;
+// const MIME_TYPE = "image/jpeg";
+// const QUALITY = 0.7;
+
+// const input = document.getElementById("img-input");
+// input.onchange = function (ev) {
+//   const file = ev.target.files[0]; // get the file
+//   const blobURL = URL.createObjectURL(file);
+//   const img = new Image();
+//   img.src = blobURL;
+//   img.onerror = function () {
+//     URL.revokeObjectURL(this.src);
+//     // Handle the failure properly
+//     console.log("Cannot load image");
+//   };
+//   img.onload = function () {
+//     URL.revokeObjectURL(this.src);
+//     const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
+//     const canvas = document.createElement("canvas");
+//     canvas.width = newWidth;
+//     canvas.height = newHeight;
+//     const ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0, newWidth, newHeight);
+//     canvas.toBlob(
+//       (blob) => {
+//         // Handle the compressed image. es. upload or save in local state
+//         displayInfo('Original file', file);
+//         displayInfo('Compressed file', blob);
+//       },
+//       MIME_TYPE,
+//       QUALITY
+//     );
+//     document.getElementById("root").append(canvas);
+//   };
+// };
+
+// function calculateSize(img, maxWidth, maxHeight) {
+//   let width = img.width;
+//   let height = img.height;
+
+//   // calculate the width and height, constraining the proportions
+//   if (width > height) {
+//     if (width > maxWidth) {
+//       height = Math.round((height * maxWidth) / width);
+//       width = maxWidth;
+//     }
+//   } else {
+//     if (height > maxHeight) {
+//       width = Math.round((width * maxHeight) / height);
+//       height = maxHeight;
+//     }
+//   }
+//   return [width, height];
 // }
 
-// function getUploadImageUrl_2(input) {
-
-//     if (input.files && input.files[0]) {
-//         var reader = new FileReader();
-
-//         reader.onload = function (e) {
-//             document.getElementById('avatar_preview_2').setAttribute('src', e.target.result);
-//         };
-
-//         reader.readAsDataURL(input.files[0]);
-//     }
-// }
-
-
-
-// //image 3
-// var input = document.getElementById('avatar_3');
-// if (input) {
-//     input.addEventListener('change', function () {
-//         getUploadImageUrl_3(this);
-//         //sendImage();
-//     });
-// }
-
-// function getUploadImageUrl_3(input) {
-
-//     if (input.files && input.files[0]) {
-//         var reader = new FileReader();
-
-//         reader.onload = function (e) {
-//             document.getElementById('avatar_preview_3').setAttribute('src', e.target.result);
-//         };
-
-//         reader.readAsDataURL(input.files[0]);
-//     }
-// }
-
-// $("#uploader").click(function () {
-//     event.preventDefault();
-//     $("#uploader").html("please wait...");
-//     $(this).attr("disabled", true);
-//     var thisPost = {};
-//     thisPost.title = $("#title").val();
-//     thisPost.description = $("#description").val();
-//     thisPost.price = $("#price").val();
-//     var image = document.getElementById('avatar_preview').getAttribute('src');
-//     var image2 = document.getElementById('avatar_preview_2').getAttribute('src');
-//     var image3 = document.getElementById('avatar_preview_3').getAttribute('src');
-//     thisPost.image = image.split(';base64,')[1];
-//     thisPost.image2 = image2 == "" ? thisPost.image : image2.split(';base64,')[1];
-//     thisPost.image3 = image3 == "" ? thisPost.image : image3.split(';base64,')[1];
-
-
-//     if (thisPost.title == "" || thisPost.description == "" || image == "") {
-//         alert("Important fields must not be empty");
-//         $("#uploader").attr("disabled", false);
-//         $("#uploader").html("SUBMIT");
-//     }
-//     else {
-//         $.ajax({
-//             url: "/admin/upload",
-//             method: "POST",
-//             data: thisPost,
-//             success: function (data) {
-//                 data = JSON.parse(data);
-//                 window.location = "/admin/home";
-//                 alert('uploaded successfully');
-//                 //$("#uploader").attr("disabled", false);
-//             },
-//             error: function (XMLHttpRequest, textStatus, errorThrown) {
-//                 alert(textStatus + ', ' + errorThrown);
-//                 $("#uploader").attr('disabled', false);
-//                 $("#uploader").html("Submit");
-//             }
-//         })
-//     }
-// });
 
 
 
